7e3dd90a76844d6f478326324b241f13cf9012cb
Sarvar Kuzibaev <kuzibaevs@yandex.ru>
2022-08-08 23:03:45 +0300

    bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,31 @@
 const popUp = document.querySelector('.pop-up')
 const editButton = document.querySelector('.btn_type_edit')
 const closeButton = popUp.querySelector('.btn_type_close')
-const popUpForm = popUp.querySelector('.pop-up__container')
-const submitButton = popUpForm.querySelector('.btn_type_submit')
+const popUpContainer = popUp.querySelector('.pop-up__container')
+const popUpForm = popUpContainer.querySelector('.pop-up__form')
+const submitButton = popUpContainer.querySelector('.btn_type_submit')
+let nameInput = popUpForm.querySelector('.pop-up__input_type_name')
+let infoInput = popUpForm.querySelector('.pop-up__input_type_info')
 const profileInfo = document.querySelector('.profile__info')
-const profileName = profileInfo.querySelector('.profile__name')
-const profileDescription = profileInfo.querySelector('.profile__description')
-const nameInput = popUpForm.querySelector('.pop-up__input_type_name').value
-const infoInput = popUpForm.querySelector('.pop-up__input_type_info').value
+let profileName = profileInfo.querySelector('.profile__name')
+let profileDescription = profileInfo.querySelector('.profile__description')
 
 const togglePopUp = function () {
     popUp.classList.toggle('pop-up_open')
+    nameInput.setAttribute('value', 'Жак-Ив Кусто')
+    infoInput.setAttribute('value', 'Исследователь океана')
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault()
-    profileName.textContent = nameInput
-    profileDescription.textContent = infoInput
+    profileName.textContent = nameInput.value
+    profileDescription.textContent = infoInput.value
+    togglePopUp()
 }
 
 editButton.addEventListener('click', togglePopUp)
-popUpForm.addEventListener('submit', formSubmitHandler)
 closeButton.addEventListener('click', togglePopUp)
-// submitButton.addEventListener('click', togglePopUp)
+popUpForm.addEventListener('submit', formSubmitHandler)
+
+
+
