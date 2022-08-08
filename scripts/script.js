f27c9f05ec511eee15de0abb07da6a1a7f5dcba3
Sarvar Kuzibaev <kuzibaevs@yandex.ru>
2022-08-08 21:14:20 +0300

    bug fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 const popUp = document.querySelector('.pop-up')
 const editButton = document.querySelector('.btn_type_edit')
 const closeButton = popUp.querySelector('.btn_type_close')
-const popUpForm = popUp.querySelector('.pop-up__form')
+const popUpForm = popUp.querySelector('.pop-up__container')
 const submitButton = popUpForm.querySelector('.btn_type_submit')
 const profileInfo = document.querySelector('.profile__info')
-
+const profileName = profileInfo.querySelector('.profile__name')
+const profileDescription = profileInfo.querySelector('.profile__description')
+const nameInput = popUpForm.querySelector('.pop-up__input_type_name').value
+const infoInput = popUpForm.querySelector('.pop-up__input_type_info').value
 
 const togglePopUp = function () {
-     popUp.classList.toggle('pop-up_open')
+    popUp.classList.toggle('pop-up_open')
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault()
-    const nameInput = popUpForm.querySelector('.pop-up__input_type_name').value
-    const infoInput = popUpForm.querySelector('.pop-up__input_type_info').value
-    const profileName = profileInfo.querySelector('.profile__name')
-    const profileDescription = profileInfo.querySelector('.profile__description')
     profileName.textContent = nameInput
     profileDescription.textContent = infoInput
 }
 
 editButton.addEventListener('click', togglePopUp)
 popUpForm.addEventListener('submit', formSubmitHandler)
-submitButton.addEventListener('click', togglePopUp)
 closeButton.addEventListener('click', togglePopUp)
+// submitButton.addEventListener('click', togglePopUp)
