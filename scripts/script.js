231cb4f09373f8b96f077c3b802ec1a4176d98f5
Sarvar Kuzibaev <kuzibaevs@yandex.ru>
2022-08-11 23:47:14 +0300

    js changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ const editButton = document.querySelector('.btn_type_edit')
 const closeButton = popUp.querySelector('.btn_type_close')
 const popUpContainer = popUp.querySelector('.pop-up__container')
 const popUpForm = popUpContainer.querySelector('.pop-up__form')
-let nameInput = popUpForm.querySelector('.pop-up__input_type_name')
-let infoInput = popUpForm.querySelector('.pop-up__input_type_info')
+const nameInput = popUpForm.querySelector('.pop-up__input_type_name')
+const infoInput = popUpForm.querySelector('.pop-up__input_type_info')
 const profileInfo = document.querySelector('.profile__info')
-let profileName = profileInfo.querySelector('.profile__name')
-let profileDescription = profileInfo.querySelector('.profile__description')
+const profileName = profileInfo.querySelector('.profile__name')
+const profileDescription = profileInfo.querySelector('.profile__description')
 
 const togglePopUp = function () {
     if (!popUp.classList.contains('pop-up_open')) {
@@ -19,7 +19,7 @@ const togglePopUp = function () {
 //Уважаемый ревьюер, привет! Исправил согласно вашему замечанию,
 //но, к сожалению, не особо понял саму строчку с методом контейнс.
 //Логика в принципе ясна, только вот зачем надо ставить ! - не понимаю :)
-//Буду благодарен, если вы объясните зачем нужен этот оператор. 
+//Буду благодарен, если вы объясните зачем нужен этот оператор.
 
 function formSubmitHandler (evt) {
     evt.preventDefault()
